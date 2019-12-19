8975c1aa6a1cb23557184f95994015a2e6d747d1
Travis Molter <travismolter@gmail.com>
2019-12-19 14:09:57 -0800

    Got my user model and first signup route created and tested

---
+++ __tests__/app.test.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('creates a user', () =>{
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'treemo', password: 'hello' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'treemo',
+          __v: 0
+        });
+      });
+  });
+});
